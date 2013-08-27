4c4fb6a9195da6e3150cf5dabb3e62225c5c7d25
Ashok Menon <amenon94@gmail.com>
2013-08-27 13:27:09 +0100

    Connection: Added support for caching

--- lib/heroku/conn.rb
+++ lib/heroku/conn.rb
@@ -7,19 +7,36 @@ class Heroku::Conn
     https.verify_mode = OpenSSL::SSL::VERIFY_NONE
   end
 
-  def self.method_missing(method, end_point, body=nil)
-    _Request = Net::HTTP.const_get(method.capitalize)
-    req      = _Request.new(end_point, headers)
-    req.body = body
+  @response_cache = {}
 
-    @https.request(req)
+  def self.method_missing(method, end_point, opts = {})
+    _Request    = Net::HTTP.const_get(method.capitalize)
+
+    header_hash = headers(opts[:etag] ? { "If-None-Match" => opts[:etag] } : nil)
+
+    req         = _Request.new(end_point, header_hash)
+    req.body    = opts[:body]
+
+    check_response(@https.request(req))
   end
 
-  def self.headers
+private
+
+  def self.check_response(res)
+    case res
+    when Net::HTTPOK
+      @response_cache[res["ETag"]] = res if res["ETag"]
+      res
+    when Net::HTTPNotModified
+      @response_cache.fetch(res["ETag"])
+    end
+  end
+
+  def self.headers(additional = nil)
     {
       "Accept"        => 'application/vnd.heroku+json; version=3',
       "Authorization" => Heroku::Config.auth_token,
       "User-Agent"    => Heroku::Config::USER_AGENT
-    }
+    }.merge(additional || {})
   end
 end
