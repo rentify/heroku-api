279fd003f96053644a08fa0837d8244f4d298e6f
Ashok Menon <amenon94@gmail.com>
2014-06-23 09:57:17 +0100

    Ruby 2: Added Testing for Heroku::Conn#parse_body
    Net::HTTP#request does not automatically perform gzip reading any more
    for some reason, so I am dealing with encoding at this level to fix
    this. (Net::HTTP#get *does* however decompress the body)

--- spec/heroku/conn_spec.rb
+++ spec/heroku/conn_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Heroku::Conn do
-
   describe ".check_response" do
     context "when the response is unsuccessful" do
       let(:unsuccessful_response) { Net::HTTPClientError.new() }
@@ -9,6 +8,58 @@ describe Heroku::Conn do
       it "should raise a error" do
         expect { described_class.send(:check_response, nil, nil, response) }.to raise_error
       end
+    end
+  end
+
+  describe ".parse_body" do
+    let(:hash) { {"lorem" => "ipsum dolor sit amet"} }
+    let(:body) { hash.to_json }
+    let(:res)  { double(:res) }
+
+    def self.it_returns_the_hash
+      it "returns the hash" do
+        expect(described_class.send(:parse_body, res)).to eq(hash)
+      end
+    end
+
+    def set_body(res, body)
+      allow(res).to receive(:body).and_return(body)
+    end
+
+    def set_encoding(res, enc)
+      allow(res).to receive(:[])
+        .with("content-encoding")
+        .and_return(enc)
+    end
+
+    context "when the body is not encoded" do
+      before do
+        set_encoding(res, "identity")
+        set_body(res, body)
+      end
+
+      it_returns_the_hash
+    end
+
+    context "when the body is deflated" do
+      before do
+        set_encoding(res, "deflate")
+        set_body(res, Zlib::Deflate.deflate(body))
+      end
+
+      it_returns_the_hash
+    end
+
+    context "when the body is gzipped" do
+      before do
+        set_encoding(res, "gzip")
+
+        strio = StringIO.new
+        Zlib::GzipWriter.wrap(strio) { |gz| gz.write(body) }
+        set_body(res, strio.string)
+      end
+
+      it_returns_the_hash
     end
   end
 end
